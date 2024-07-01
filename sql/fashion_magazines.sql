ae2401e8b61c7472dd0a2c15431da6c12adae2ee
bigbdawson <bigbdawson@gmail.com>
2024-07-01 01:16:04 -0400

    Fashion

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,8 @@
--- Add your sql here
+SELECT c.customer_name AS Customer, 
+       PRINTF("$%.2f", SUM(s.price_per_month * s.subscription_length)) AS "Amount Due"
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN subscriptions s ON o.subscription_id = s.subscription_id
+WHERE o.order_status <> 'paid'
+  AND s.description = 'Fashion Magazine'
+GROUP BY c.customer_name;
